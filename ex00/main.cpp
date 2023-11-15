f994b8acd84f9ddea0b6f993b72912352f5c14cb
jaguar-ks <fahdamineksouss@gmail.com>
2023-11-15 02:58:40 +0100

    FIX : fixed an error in the make file and regenrate the main file after losing it

---
+++ ex00/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faksouss <faksouss@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 02:25:51 by faksouss          #+#    #+#             */
+/*   Updated: 2023/11/15 02:57:29 by faksouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+#include <cstdlib>
+#include <exception>
+#include <sys/_types/_size_t.h>
+#include <vector>
+
+int main(){
+    {
+        std::vector<int> Vc;
+        srand(time(NULL));
+        for (int i = 0; i < 50; i++)
+            Vc.push_back(rand() % 100);
+        for (size_t i = 0; i < Vc.size(); i++)
+            std::cout << Vc.at(i) << std::endl;
+        try {
+            std::vector<int>::iterator it;
+            it = easyfind<int>(Vc, rand() % 100);
+            std::cout << "element fond [" << *it << "]" << std::endl;
+        }
+        catch (std::exception &e){
+            std::cerr << e.what() << std::endl;
+        }
+    }
+}
